dc464047dc000e033f8d190046407fdf5826ecb8
masx200 <34191203+masx200@users.noreply.github.com>
2022-02-25 13:13:31 +0800

    Update getsetPheromones.ts

--- src/getsetPheromones.ts
+++ src/getsetPheromones.ts
@@ -10,7 +10,9 @@ export function getPheromones(left: number, right: number) {
 /* 修改信息素 */
 export function setPheromones(left: number, right: number, Pheromone: number) {
     //参数排序
-    let sorted = [left, right].sort((a, b) => a - b);
-    Pheromonesrecord.set(`${sorted[0]},${sorted[1]}`, Pheromone);
+    //信息素参数不分正反
+    let max = Math.max(left, right);
+    let min = Math.min(left, right);
+    Pheromonesrecord.set(`${min},${max}`, Pheromone);
 }
 console.log(Pheromonesrecord);
