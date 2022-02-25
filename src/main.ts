9ae9cb3352de71d8cdd53d16b096e589cde2dba7
masx200 <34191203+masx200@users.noreply.github.com>
2022-02-25 17:59:44 +0800

    格式化

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,3 @@ app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
-
--- src/testgeteuclideandistance.ts
+++ src/testgeteuclideandistance.ts
@@ -1,31 +1,31 @@
 import {
-    euclideandistance,
-    geteuclideandistancebyindex,
+  euclideandistance,
+  geteuclideandistancebyindex,
 } from "./geteuclideandistance";
 import { Nodecoordinates } from "./Nodecoordinates";
 
 export function testgeteuclideandistance() {
-    console.log("testgeteuclideandistance test start");
-    console.assert(euclideandistance([3, 4], [0, 0]) === 5);
+  console.log("testgeteuclideandistance test start");
+  console.assert(euclideandistance([3, 4], [0, 0]) === 5);
 
-    let nodecoordinates1: Nodecoordinates = [
-        [0, 0],
-        [1, 4],
-        [5, 5],
-        [6, 8],
-    ];
-    console.assert(geteuclideandistancebyindex(0, 3, nodecoordinates1) === 10);
-    console.assert(geteuclideandistancebyindex(3, 0, nodecoordinates1) === 10);
-    console.assert(geteuclideandistancebyindex(2, 2, nodecoordinates1) === 0);
+  let nodecoordinates1: Nodecoordinates = [
+    [0, 0],
+    [1, 4],
+    [5, 5],
+    [6, 8],
+  ];
+  console.assert(geteuclideandistancebyindex(0, 3, nodecoordinates1) === 10);
+  console.assert(geteuclideandistancebyindex(3, 0, nodecoordinates1) === 10);
+  console.assert(geteuclideandistancebyindex(2, 2, nodecoordinates1) === 0);
 
-    let nodecoordinates2: Nodecoordinates = [
-        [0, 0],
-        [1, 4],
-        [6, 8],
-        [5, 5],
-    ];
-    console.assert(geteuclideandistancebyindex(0, 2, nodecoordinates2) === 10);
-    console.assert(geteuclideandistancebyindex(2, 0, nodecoordinates2) === 10);
-    console.assert(geteuclideandistancebyindex(1, 1, nodecoordinates2) === 0);
-    console.log("testgeteuclideandistance test ok");
+  let nodecoordinates2: Nodecoordinates = [
+    [0, 0],
+    [1, 4],
+    [6, 8],
+    [5, 5],
+  ];
+  console.assert(geteuclideandistancebyindex(0, 2, nodecoordinates2) === 10);
+  console.assert(geteuclideandistancebyindex(2, 0, nodecoordinates2) === 10);
+  console.assert(geteuclideandistancebyindex(1, 1, nodecoordinates2) === 0);
+  console.log("testgeteuclideandistance test ok");
 }
