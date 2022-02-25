48b66d64d88b115591f1c80c0f99b05cab3909c9
masx200 <masx200@qq.com>
2022-02-25 06:51:26 +0000

    getstoreofnodecoordinates

---
+++ src/geteuclideandistance.ts
@@ -0,0 +1,71 @@
+import * as math from "mathjs";
+import { Nodecoordinates } from "./Nodecoordinates";
+const nodecoordinatestostore = new WeakMap<Nodecoordinates, Map<
+    `${number},${number}`,
+    number
+> >();
+
+function getstoreofnodecoordinates(nodecoordinates: Nodecoordinates): Map<
+    `${number},${number}`,
+    number
+> {
+    return (
+        nodecoordinatestostore.get(nodecoordinates) ??
+        (() => {
+            const euclideandistancerecord = new Map<
+    `${number},${number}`,
+    number
+>();
+            nodecoordinatestostore.set(nodecoordinates, euclideandistancerecord);
+            
+            return euclideandistancerecord;
+        })()
+    );
+}
+console.log(nodecoordinatestostore);
+
+/* 获得欧式距离 */
+export function geteuclideandistancebyindex(
+    left: number,
+    right: number,
+    nodecoordinates: Nodecoordinates
+): number {
+    //参数排序
+    //距离参数不分正反
+    const euclideandistancerecord = getstoreofnodecoordinates(nodecoordinates);
+    console.log(euclideandistancerecord);
+    return (
+        euclideandistancerecord.get(
+            `${left},${right}`
+        ) ??
+        euclideandistancerecord.get(
+            `${right},${left}`
+        ) ??
+        (() => {
+            let leftpair = nodecoordinates[left];
+            let rightpair = nodecoordinates[right];
+            let distance = euclideandistance(
+                leftpair,
+
+                rightpair
+            );
+            let max = Math.max(left, right);
+            let min = Math.min(left, right);
+
+            euclideandistancerecord.set(
+                `${min},${max}`,
+                distance
+            );
+            return distance;
+        })()
+    );
+}
+export function euclideandistance(
+    leftpair: [number, number],
+    rightpair: [number, number]
+): number {
+    return Number(math.distance(leftpair, rightpair));
+}
+
+
+
