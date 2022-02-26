a6636365d25ffe56d98a30c05f69c11b9a910b6b
masx200 <34191203+masx200@users.noreply.github.com>
2022-02-26 18:55:19 +0800

    修复了回环双向相等路径

--- functions/ispathsequalinbothdirections.ts
+++ functions/ispathsequalinbothdirections.ts
@@ -4,8 +4,52 @@ export function ispathsequalinbothdirections(
     left: Array<number>,
     right: Array<number>
 ): boolean {
-    return isEqual(left, right) || isEqual(reversearray(left), right);
+    if (!left.length) {
+        return false;
+    }
+    if (!right.length) {
+        return false;
+    }
+
+    return (
+        cyclereorganizeequal(left, right) ||
+        cyclereorganizeequal(left, reversearray(right))
+    );
 }
 function reversearray<T>(oldarr: Array<T>): Array<T> {
     return Array.from(oldarr).reverse();
+}
+// function reverseequal(left: Array<number>, right: Array<number>): boolean {
+//     if (!left.length) {
+//         return false;
+//     }
+//     if (!right.length) {
+//         return false;
+//     }
+//     return (
+//         isEqual(left, right) ||
+//         cyclereorganizeequal(left, right) ||
+//         cyclereorganizeequal(reversearray(left), right)
+//     );
+// }
+function cyclereorganizeequal(
+    left: Array<number>,
+    right: Array<number>
+): boolean {
+    if (!left.length) {
+        return false;
+    }
+    if (!right.length) {
+        return false;
+    }
+    
+    const firstindexinright = right.findIndex((v) => v === left[0]);
+    if (firstindexinright < 0) {
+        return false;
+    }
+    const reorganizedright = right
+        .slice(firstindexinright)
+        .concat(right.slice(0, firstindexinright));
+console.log(left, right, reorganizedright);
+    return isEqual(left, right) || isEqual(left, reorganizedright);
 }
--- test/testpathsequalinbothdirections.ts
+++ test/testpathsequalinbothdirections.ts
@@ -7,9 +7,14 @@ export function testpathsequalinbothdirections() {
     console.assert(ispathsequalinbothdirections([1, 2, 3], [1, 2, 3]));
     console.assert(!ispathsequalinbothdirections([1, 2, 3], [0, 10, 100]));
     console.assert(!ispathsequalinbothdirections([1, 2, 3], [3, 2, 1, 4]));
-        console.assert(ispathsequalinbothdirections([1, 2, 3], [3,1,2]));
-    console.assert(ispathsequalinbothdirections([1, 2, 3,4], [4,1, 2, 3]));
-        console.assert(ispathsequalinbothdirections([1, 2, 3,4], [4,3, 2, 1]));
-            
+    console.assert(ispathsequalinbothdirections([1, 2, 3], [3, 1, 2]));
+    console.assert(ispathsequalinbothdirections([1, 2, 3, 4], [4, 1, 2, 3]));
+    console.assert(ispathsequalinbothdirections([1, 2, 3, 4], [4, 3, 2, 1]));
+
+
+    console.assert(ispathsequalinbothdirections([0,1, 2, 3, 4], [0,4, 3, 2, 1]));
+    console.assert(ispathsequalinbothdirections([4,0,1, 2, 3], [0,4, 3, 2, 1]));
+    console.assert(ispathsequalinbothdirections([1, 2, 3, 4,0], [4, 3, 2, 1,0]));
+    console.assert(ispathsequalinbothdirections([ 2, 3, 4,0,1], [4, 3, 2, 1,0]));
     console.log("test pathsequalinbothdirections ok");
 }
