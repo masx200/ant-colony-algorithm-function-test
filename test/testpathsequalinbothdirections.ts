e81e51bb92daddd04019b6b5a4c90324590ad7c0
masx200 <masx200@qq.com>
2022-02-26 10:00:20 +0000

    update test/testpathsequalinbothdirections.ts.

--- test/testpathsequalinbothdirections.ts
+++ test/testpathsequalinbothdirections.ts
@@ -1,11 +1,11 @@
-import { pathsequalinbothdirections } from "../functions/ispathsequalinbothdirections";
+import { ispathsequalinbothdirections } from "../functions/ispathsequalinbothdirections";
 
 export function testpathsequalinbothdirections() {
     console.log("test pathsequalinbothdirections start");
 
-    console.assert(pathsequalinbothdirections([1, 2, 3], [3, 2, 1]));
-    console.assert(pathsequalinbothdirections([1, 2, 3], [1, 2, 3]));
-    console.assert(!pathsequalinbothdirections([1, 2, 3], [0, 0, 0]));
-    console.assert(!pathsequalinbothdirections([1, 2, 3], [3, 2, 1, 4]));
+    console.assert(ispathsequalinbothdirections([1, 2, 3], [3, 2, 1]));
+    console.assert(ispathsequalinbothdirections([1, 2, 3], [1, 2, 3]));
+    console.assert(!ispathsequalinbothdirections([1, 2, 3], [0, 0, 0]));
+    console.assert(!ispathsequalinbothdirections([1, 2, 3], [3, 2, 1, 4]));
     console.log("test pathsequalinbothdirections ok");
 }
