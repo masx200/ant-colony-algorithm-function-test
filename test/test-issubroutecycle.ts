9a3ad3a7bc4d8668db886a2e8eb2f3bdca741bae
masx200 <34191203+masx200@users.noreply.github.com>
2022-02-28 13:55:25 +0800

    Update test-issubroutecycle.ts

--- test/test-issubroutecycle.ts
+++ test/test-issubroutecycle.ts
@@ -16,5 +16,6 @@ export function test_issubroutecycle() {
     console.assert(issubroutecycle([1, 2, 3, 4, 5, 6, 7, 8], [5, 4, 3, 2], 8));
     console.assert(issubroutecycle([1, 2, 3, 4, 5, 6, 7, 8], [2, 3, 4, 5], 9));
     console.assert(issubroutecycle([1, 2, 3, 4, 5, 6, 7, 8], [5, 4, 3, 2], 9));
+    console.assert(!issubroutecycle([1, 2, 3, 4, 5], [4, 2, 1, 5, 3], 5));
     console.log("test_issubroutecycle ok");
 }
