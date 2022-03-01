61f9b94cd28e18eb907a8e5b95a54cb92ec86185
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-01 23:04:36 +0800

    Create nodecoordinates4.ts

---
+++ test/nodecoordinates4.ts
@@ -0,0 +1,13 @@
+import { Nodecoordinates } from "../functions/Nodecoordinates";
+
+export const nodecoordinates4: Nodecoordinates = [
+    [0, 25],
+    [25, 0],
+    [75, 0],
+    [0, 75],
+    [25, 75],
+    [75, 25],
+    [100, 75],
+    [75, 100],
+    
+];
