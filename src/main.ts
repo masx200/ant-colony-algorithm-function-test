613ec99585aca2a1551a0d82dbf6c5d38f42a0eb
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-04 06:34:02 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,5 @@ import "./mytestinsert";
 import "../test/index";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+app.innerHTML = `<h1>Hello Vite!</h1>`;
 document.title = "ant-colony-algorithm-function-test";
