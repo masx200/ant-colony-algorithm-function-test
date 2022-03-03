5a9d2a66268caeb28c039c3ba629e293911eeaab
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-03 08:38:14 +0800

    Update intersectionfilter.ts

--- functions/intersectionfilter.ts
+++ functions/intersectionfilter.ts
@@ -47,3 +47,10 @@ const countofnodes=nodecoordinates.length
         return result;
     }
 }
+function haverepetitions(arr:number[]):boolean{
+
+const set=new Set(arr)
+return set.size!=arr.length
+
+
+}
