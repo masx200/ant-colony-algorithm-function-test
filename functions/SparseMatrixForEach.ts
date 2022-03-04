24e9aaee8b92c594cda916dd0bb888f365d22631
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-04 19:37:45 +0800

    Update SparseMatrixForEach.ts

--- functions/SparseMatrixForEach.ts
+++ functions/SparseMatrixForEach.ts
@@ -1,1 +1,13 @@
-export function SparseMatrixForEach() {}
+import { matrixkeyiterator } from "./matrixkeyiterator";
+import { SparseTwoDimensionalMatrix } from "./SparseTwoDimensionalMatrix";
+
+export function SparseMatrixForEach(
+    matrix: SparseTwoDimensionalMatrix,
+    callback: (value: number, row: number, column: number) => void
+): void {
+    const { row, column } = matrix;
+    for (let [i, j] of matrixkeyiterator(row, column)) {
+        const value = matrix.get(i, j);
+        callback(value, i, j);
+    }
+}
