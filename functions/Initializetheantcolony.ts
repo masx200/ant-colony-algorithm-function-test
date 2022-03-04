0afdb521c7d6f1e1d6ed74d76ab77fb20b090f1d
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-04 12:48:20 +0800

    Update Initializetheantcolony.ts

--- functions/Initializetheantcolony.ts
+++ functions/Initializetheantcolony.ts
@@ -1,4 +1,5 @@
 import { closedtotalpathlength } from "./closed-total-path-length";
+import { fill_sparse_two_dimensional_matrix } from "./fill_sparse_two_dimensional_matrix";
 import { Greedyalgorithmtosolvetspwithallstartbest } from "./Greedyalgorithmtosolvetspwithallstartbest";
 import { Nodecoordinates } from "./Nodecoordinates";
 import { SparseTwoDimensionalMatrixSymmetry } from "./SparseTwoDimensionalMatrixSymmetry";
@@ -13,12 +14,24 @@ export type InitializeOptions = {
 
 /* 初始化信息素,贪心算法得到最优路径和最短路径长度 */
 export function Initializetheantcolony(opts: InitializeOptions) {
-    const { nodecoordinates } = opts;
-    console.log(opts);
+    const {
+        nodecoordinates,
+        getbestpathlength,
+        setbestpathlength,
+        setbestroute,
+        pheromonestore,
+    } = opts;
+    // console.log(opts);
     const greedypath =
         Greedyalgorithmtosolvetspwithallstartbest(nodecoordinates);
     console.log("贪心算法得到的路径是", greedypath);
 
     const totallength = closedtotalpathlength(greedypath, nodecoordinates);
     console.log("贪心算法得出的路径长度", totallength);
+    if (totallength < getbestpathlength()) {
+        setbestpathlength(totallength);
+        setbestroute(greedypath);
+    }
+    const initialpheromone = 1 / nodecoordinates.length / totallength;
+    fill_sparse_two_dimensional_matrix(pheromonestore, initialpheromone);
 }
