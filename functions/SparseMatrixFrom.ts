e5a5fb8867815d86ace20d3c7988eb99b9f5cded
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-04 19:40:04 +0800

    Update SparseMatrixFrom.ts

--- functions/SparseMatrixFrom.ts
+++ functions/SparseMatrixFrom.ts
@@ -1,1 +1,17 @@
-export function SparseMatrixFrom() {}
+import { SparseMatrixCreate } from "./SparseMatrixCreate";
+import { SparseTwoDimensionalMatrix } from "./SparseTwoDimensionalMatrix";
+
+export function SparseMatrixFrom(
+    matrix: SparseTwoDimensionalMatrix
+): SparseTwoDimensionalMatrix {
+    const { row, column } = matrix;
+
+    const obj = SparseMatrixCreate({
+        row,
+        column,
+        initializer(i, j) {
+            return matrix.get(i, j);
+        },
+    });
+    return obj;
+}
