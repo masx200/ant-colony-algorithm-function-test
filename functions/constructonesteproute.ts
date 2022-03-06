5bb5c9280958583ccf25d90167a96bf88823c010
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-06 17:47:09 +0800

    Update constructonesteproute.ts

--- functions/constructonesteproute.ts
+++ functions/constructonesteproute.ts
@@ -131,6 +131,8 @@ export function constructonesteproute({
             return route;
         }
         if (
+            //如果所有城市都经过了,则结束了,如果没走完所有城市,则计算部分城市的环路路径长度
+            route.length < countofnodes &&
             route.length >= 2 &&
             closedtotalpathlength({
                 // countofnodes: route.length,
@@ -138,30 +140,40 @@ export function constructonesteproute({
                 getdistancebyindex: getdistancebyserialnumber,
             }) > getbestlength()
         ) {
+            //测试不接受次优解
+            pathTabooList.add([...route, nextnode]);
+            console.warn(
+                "路径构建失败,路径片段长度已经大于最优路径长度,禁忌此路径",
+                [...route, nextnode]
+            );
+            //广度搜索
             console.log(
                 "接受次优解的概率为",
                 probabilityofacceptingasuboptimalsolution
             );
-            if (Math.random() < probabilityofacceptingasuboptimalsolution) {
-                console.warn(
-                    "接受次优解，路径片段长度已经大于最优路径长度，路径构建经过节点",
-                    [...route, nextnode]
-                );
-                return [...route, nextnode];
-            } else {
-                /* .在构建路径过程中,如果当前路径片段总长度已经大于最优解的长度,则停止此路径搜索,并把路径片段加入路径禁忌列表中. */
-                pathTabooList.add([...route, nextnode]);
-                console.warn(
-                    "路径构建失败,路径片段长度已经大于最优路径长度,禁忌此路径",
-                    [...route, nextnode]
-                );
-                //广度搜索
-                return [startnode];
-
-                //route = route.slice();
-                // debugger;
-                // return route;
-            }
+            return [startnode];
+            // }
+            // {
+            //             if (Math.random() < probabilityofacceptingasuboptimalsolution) {
+            //                 console.warn(
+            //                     "接受次优解，路径片段长度已经大于最优路径长度，路径构建经过节点",
+            //                     [...route, nextnode]
+            //                 );
+            //                 return [...route, nextnode];
+            //             } else {
+            //                 /* .在构建路径过程中,如果当前路径片段总长度已经大于最优解的长度,则停止此路径搜索,并把路径片段加入路径禁忌列表中. */
+            //                 pathTabooList.add([...route, nextnode]);
+            //                 console.warn(
+            //                     "路径构建失败,路径片段长度已经大于最优路径长度,禁忌此路径",
+            //                     [...route, nextnode]
+            //                 );
+            //                 //广度搜索
+            //                 return [startnode];
+            // }
+            //route = route.slice();
+            // debugger;
+            // return route;
+            // }
         } else {
             route = [...route, nextnode];
             console.log("路径构建正常经过节点", route);
