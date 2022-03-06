34a945f1fca824e190e66e3441b3d8221b3fbad9
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-06 18:15:57 +0800

    Create nodecoordinates16.ts

---
+++ test/nodecoordinates16.ts
@@ -0,0 +1,11 @@
+import { Nodecoordinates } from "../functions/Nodecoordinates";
+
+export const nodecoordinates6: Nodecoordinates = [
+    [0, 25],
+    [25, 0],
+    [75, 0],
+    [0, 75],
+
+    [100, 75],
+    [75, 100],
+];
