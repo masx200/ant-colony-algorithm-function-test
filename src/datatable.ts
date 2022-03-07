78dc013fbfbdd9cf7ed9466fc23fad5cf1c9f7be
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-07 20:32:36 +0800

    Update datatable.ts

--- src/datatable.ts
+++ src/datatable.ts
@@ -1,9 +1,11 @@
-import { defineComponent } from "vue";
-
+import { defineComponent, Ref, ref } from "vue";
+const tablebody: Ref<[number, number, number, number, string][]> = ref<
+    [number, number, number, number, string][]
+>([]);
 export default defineComponent({
     setup() {
         const tableheads = ["序号", "长度", "循环次数", "耗时秒", "路径"];
-        const tablebody: [number, number, number, number, string][] = [];
+
         return { tableheads, tablebody };
     },
 });
