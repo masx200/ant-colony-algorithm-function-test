91329ba126dc91dc5c87e22e4caac5bd129bf8bb
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-08 10:50:22 +0800

    @masx200/event-emitter-target

--- functions/createTSPrunner.ts
+++ functions/createTSPrunner.ts
@@ -1,6 +1,33 @@
+import EventEmitterTargetClass from "@masx200/event-emitter-target";
+import { SparseMatrixSymmetry } from "../matrixtools/SparseMatrixSymmetry";
 import { createPathTabooList } from "./createPathTabooList";
 import { createPheromonestore } from "./createPheromonestore";
 import { Nodecoordinates } from "./Nodecoordinates";
+import { PathTabooList } from "./PathTabooList";
+const finishalliterationsflag = Symbol();
+const finishonerouteflag = Symbol();
+const finishoneiterationflag = Symbol();
+interface TSPRunner {
+    onfinishalliterations: (callback: (data: DataOfFinishAllIteration) => void) => void;
+    runiterations: (iterations: number) => void;
+    onfinishoneiteration: (callback: (data: DataOfFinishOneIteration) => void) => void;
+    onfinishoneroute: (callback: (data: DataOfFinishOneRoute) => void) => void;
+    getlengthofstagnant: () => number;
+    getnumberofiterations: () => number;
+    getnumberofstagnant: () => number;
+    getglobalbestlength: () => number;
+    getglobalbestroute: () => number[];
+    getcurrentsearchindex: () => number;
+    pheromonestore: SparseMatrixSymmetry<number>;
+    betazero: number;
+    maxnumberofstagnant: number;
+    nodecoordinates: Nodecoordinates;
+    alphazero: number;
+    searchloopcountratio: number;
+    numberofants: number;
+    maxnumberofiterations: number;
+    pathtaboolist: PathTabooList<number>;
+}
 
 export function createTSPrunner({
     nodecoordinates,
@@ -18,7 +45,7 @@ export function createTSPrunner({
     numberofants: number;
     maxnumberofiterations: number;
     maxnumberofstagnant: number;
-}) {
+}): TSPRunner {
     const countofnodes = nodecoordinates.length;
     const pathtaboolist = createPathTabooList(countofnodes);
     const pheromonestore = createPheromonestore(countofnodes);
@@ -42,8 +69,44 @@ export function createTSPrunner({
     const getnumberofiterations = () => {
         return numberofiterations;
     };
+    let lengthofstagnant = Infinity;
+    const getlengthofstagnant = () => {
+        return lengthofstagnant;
+    };
+    const emitter = EventEmitterTargetClass();
 
+    const onfinishoneroute = (
+        callback: (data: DataOfFinishOneRoute) => void
+    ) => {
+        emitter.on(finishonerouteflag, callback);
+    };
+    const emitfinishoneroute = (data: DataOfFinishOneRoute) => {
+        emitter.emit(finishonerouteflag, data);
+    };
+    const onfinishoneiteration = (
+        callback: (data: DataOfFinishOneIteration) => void
+    ) => {
+        emitter.on(finishoneiterationflag, callback);
+    };
+    const emitfinishoneiteration = (data: DataOfFinishOneIteration) => {
+        emitter.emit(finishoneiterationflag, data);
+    };
+    const runiterations = (iterations: number) => {};
+
+    const onfinishalliterations = (
+        callback: (data: DataOfFinishAllIteration) => void
+    ) => {
+        emitter.on(finishalliterationsflag, callback);
+    };
+    const emitfinishalliterations = (data: DataOfFinishAllIteration) => {
+        emitter.emit(finishalliterationsflag, data);
+    };
     const result = {
+        onfinishalliterations,
+        runiterations,
+        onfinishoneiteration,
+        onfinishoneroute,
+        getlengthofstagnant,
         getnumberofiterations,
         getnumberofstagnant,
         getglobalbestlength,
@@ -61,3 +124,6 @@ export function createTSPrunner({
     };
     return result;
 }
+export interface DataOfFinishOneRoute {}
+export interface DataOfFinishOneIteration {}
+export interface DataOfFinishAllIteration {}
