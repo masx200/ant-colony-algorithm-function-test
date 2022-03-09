ace95b4c49f97a1938ebd926bb5da551733b291e
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-09 22:04:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     root: path.resolve(__dirname, "src"),
     plugins: [vuePlugin()],
     build: {
+        outDir: path.resolve(__dirname, "dist"),
         target: "es2018",
         terserOptions: {
             compress: { drop_console: true, drop_debugger: true },
