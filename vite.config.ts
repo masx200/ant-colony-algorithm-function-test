e648ab7b8505b5d4eb6e041e36c7d2f27952f070
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-09 22:23:00 +0800

    "babel-plugin-clean-code"

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import vuePlugin from "@vitejs/plugin-vue";
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 import path from "path";
 export default defineConfig({
     root: path.resolve(__dirname, "src"),
-    plugins: [vuePlugin()],
+    plugins: [
+        vuePlugin(),
+        getBabelOutputPlugin({ plugins: ["babel-plugin-clean-code"] }),
+    ],
     build: {
         emptyOutDir: true,
         outDir: path.resolve(__dirname, "dist"),
