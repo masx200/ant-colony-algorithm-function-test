ca2e4165ea85a14c28bdfabf53de6fa3092001f5
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-09 21:54:21 +0800

    vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 import vuePlugin from "@vitejs/plugin-vue";
+import path from "path";
 export default defineConfig({
+    root: path.resolve(__dirname, "src"),
     plugins: [vuePlugin()],
-    build: { target: "es2021" },
+    build: {
+        target: "es2018",
+        terserOptions: {
+            compress: { drop_console: true, drop_debugger: true },
+        },
+    },
 });
