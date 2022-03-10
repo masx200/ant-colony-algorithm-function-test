7806c4d1c709405324e9e6036d4a21f83a34ff20
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-10 08:22:28 +0800

    Update createEventPair.ts

--- functions/createEventPair.ts
+++ functions/createEventPair.ts
@@ -2,7 +2,7 @@ import { EventEmitterTarget } from "@masx200/event-emitter-target";
 
 export function createEventPair<T = undefined>(
     emitter: EventEmitterTarget
-): {
+): { off: (callback: (data: T) => void) => void
     emit: (data?: T | undefined) => void;
     on: (callback: (data: T) => void) => void;
     event_name: symbol;
@@ -11,8 +11,11 @@ export function createEventPair<T = undefined>(
     const on = (callback: (data: T) => void) => {
         emitter.on(event_name, callback);
     };
+const off = (callback: (data: T) => void) => {
+        emitter.off(event_name, callback);
+    };
     const emit = (data?: T) => {
         emitter.emit(event_name, data);
     };
-    return { emit: emit, on, event_name };
+    return { emit: emit, on, event_name ，off};
 }
