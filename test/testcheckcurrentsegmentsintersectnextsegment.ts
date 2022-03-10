0711cd6bfc66cb60c36bc11e4e1da8be4b5426e6
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-10 11:31:11 +0800

    Update testcheckcurrentsegmentsintersectnextsegment.ts

--- test/testcheckcurrentsegmentsintersectnextsegment.ts
+++ test/testcheckcurrentsegmentsintersectnextsegment.ts
@@ -2,8 +2,28 @@ import { intersectionfilterfun } from "../functions/intersectionfilterfun";
 import { asserttrue } from "./asserttrue";
 import { nodecoordinates21 } from "./nodecoordinates21";
 import { nodecoordinates12 } from "./nodecoordinates12";
+import { intersection_filter_with_cycle_route } from "../functions/intersection_filter_with_cycle_route";
+import { nodecoordinates16 } from "./nodecoordinates16";
 
 export function testcheckcurrentsegmentsintersectnextsegment() {
+    asserttrue(
+        intersection_filter_with_cycle_route({
+            cycleroute: [8, 0, 9, 6, 5, 14, 13, 12, 11, 15, 7, 3, 1, 2, 4, 10],
+            nodecoordinates: nodecoordinates16,
+        })
+    );
+    asserttrue(
+        !intersection_filter_with_cycle_route({
+            cycleroute: [10, 11, 4, 5, 9, 3, 0, 1, 2, 6, 7, 8],
+            nodecoordinates: nodecoordinates12,
+        })
+    );
+    asserttrue(
+        intersection_filter_with_cycle_route({
+            cycleroute: [7, 6, 4, 5, 1, 2, 0, 3, 9, 11, 8, 10],
+            nodecoordinates: nodecoordinates12,
+        })
+    );
     console.log("  testcheckcurrentsegmentsintersectnextsegment start");
     asserttrue(
         intersectionfilterfun({
