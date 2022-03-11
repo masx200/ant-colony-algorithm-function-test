f2f0afa8bf071564091b637af7d032e667baeb7c
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-11 21:31:07 +0800

    TSP_workerRef.value = endpoint;

--- src/create_TSP_Worker_comlink.ts
+++ src/create_TSP_Worker_comlink.ts
@@ -3,6 +3,7 @@ import { DataOfFinishOneIteration } from "../functions/DataOfFinishOneIteration"
 import { DataOfFinishOneRoute } from "../functions/DataOfFinishOneRoute";
 import { Nodecoordinates } from "../functions/Nodecoordinates";
 import TSPWorker from "./TSP_Runner.Worker?worker";
+import { TSP_workerRef } from "./TSP_workerRef";
 import { TSP_Worker_API } from "./TSP_Worker_API";
 import { TSP_Worker_Remote } from "./TSP_Worker_Remote";
 
@@ -16,7 +17,7 @@ export async function create_TSP_Worker_comlink({
     numberofants: number;
 }): Promise<TSP_Worker_Remote> {
     const endpoint = new TSPWorker();
-
+    TSP_workerRef.value = endpoint;
     const runner = comlink.wrap<TSP_Worker_API>(endpoint);
     await runner.init_runner({
         pheromonevolatilitycoefficientR1,
@@ -38,6 +39,6 @@ export async function create_TSP_Worker_comlink({
         on_finish_one_route: { value: on_finish_one_route },
         on_finish_one_iteration: { value: on_finish_one_iteration },
     });
-    console.log(runner,remote);
+    console.log(runner, remote);
     return remote as TSP_Worker_Remote;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
-import { defineConfig } from "vite";
+import { babel, getBabelOutputPlugin } from "@rollup/plugin-babel";
 import vuePlugin from "@vitejs/plugin-vue";
-import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 import path from "path";
-export default defineConfig({
-    root: path.resolve(__dirname, "src"),
-    plugins: [
-        vuePlugin(),
-        getBabelOutputPlugin({ plugins: ["babel-plugin-clean-code"] }),
-    ],
-    build: {
-        emptyOutDir: true,
-        outDir: path.resolve(__dirname, "dist"),
-        target: "es2018",
-        terserOptions: {
-            compress: { drop_console: true, drop_debugger: true },
+import { defineConfig } from "vite";
+export default defineConfig(({ mode, command }) => {
+    console.log(mode, command);
+    return {
+        root: path.resolve(__dirname, "src"),
+        plugins: [
+            vuePlugin(),
+            getBabelOutputPlugin({ plugins: ["babel-plugin-clean-code"] }),
+            mode === "production" &&
+                command === "build" &&
+                babel({ plugins: ["babel-plugin-clean-code"] }),
+        ].filter(Boolean),
+        build: {
+            emptyOutDir: true,
+            outDir: path.resolve(__dirname, "dist"),
+            target: "es2018",
+            terserOptions: {
+                compress: { drop_console: true, drop_debugger: true },
+            },
         },
-    },
+    };
 });
