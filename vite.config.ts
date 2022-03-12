07287c662de4c5e31eaf0b42bc9fcd3fa1ed0e32
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-12 12:50:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,33 @@
-import { babel, getBabelOutputPlugin } from "@rollup/plugin-babel";
 import vuePlugin from "@vitejs/plugin-vue";
 import path from "path";
 import { defineConfig } from "vite";
 export default defineConfig(({ mode, command }) => {
     console.log(mode, command);
     return {
+        esbuild: { drop: ["console", "debugger"] },
         root: path.resolve(__dirname, "src"),
         plugins: [
-            mode === "production" &&
-                command === "build" &&
-                babel({
-                    extensions: [".ts", ".js"],
-                    plugins: [
-                        "babel-plugin-clean-code",
-                        "@babel/plugin-syntax-typescript",
-                    ],
-                }),
+            // mode === "production" &&
+            //     command === "build" &&
+            //     babel({
+            //         extensions: [".ts", ".js"],
+            //         plugins: [
+            //             "babel-plugin-clean-code",
+            //             "@babel/plugin-syntax-typescript",
+            //         ],
+            //     }),
             vuePlugin(),
 
-            getBabelOutputPlugin({ plugins: ["babel-plugin-clean-code"] }),
+            // getBabelOutputPlugin({ plugins: ["babel-plugin-clean-code"] }),
         ].filter(Boolean),
         build: {
+            minify: "esbuild",
             emptyOutDir: true,
             outDir: path.resolve(__dirname, "dist"),
             target: "es2018",
-            terserOptions: {
-                compress: { drop_console: true, drop_debugger: true },
-            },
+            // terserOptions: {
+            //     compress: { drop_console: true, drop_debugger: true },
+            // },
         },
     };
 });
