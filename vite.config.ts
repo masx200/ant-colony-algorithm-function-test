a7f784dadb86224e5597e3fcd7ddd535084443d7
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-12 15:53:16 +0800

    "babel-plugin-import",

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,31 @@
 import vuePlugin from "@vitejs/plugin-vue";
 import path from "path";
 import { defineConfig } from "vite";
+import babel from "@rollup/plugin-babel";
 export default defineConfig(({ mode, command }) => {
     console.log(mode, command);
     return {
         esbuild: { drop: ["console", "debugger"] },
         root: path.resolve(__dirname, "src"),
         plugins: [
+            vuePlugin(),
             // mode === "production" &&
             //     command === "build" &&
-            //     babel({
-            //         extensions: [".ts", ".js"],
-            //         plugins: [
-            //             "babel-plugin-clean-code",
-            //             "@babel/plugin-syntax-typescript",
-            //         ],
-            //     }),
-            vuePlugin(),
+            babel({
+                extensions: [".ts", ".js"],
+                plugins: [
+                    [
+                        "babel-plugin-import",
+                        {
+                            libraryName: "lodash",
+                            libraryDirectory: "",
+                            camel2DashComponentName: false, // default: true
+                        },
+                    ],
+                    "babel-plugin-clean-code",
+                    "@babel/plugin-syntax-typescript",
+                ],
+            }),
 
             // getBabelOutputPlugin({ plugins: ["babel-plugin-clean-code"] }),
         ].filter(Boolean),
