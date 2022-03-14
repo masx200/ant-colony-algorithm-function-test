ee3742c81df71a88a00db16aec13ba01a7dead85
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-14 22:14:16 +0800

    Update TSP_cities_map.ts

--- src/TSP_cities_map.ts
+++ src/TSP_cities_map.ts
@@ -5,17 +5,18 @@ import { nodecoordinates21 } from "../test/nodecoordinates21";
 import { nodecoordinates30 } from "../test/nodecoordinates30";
 import { nodecoordinates12 } from "../test/nodecoordinates12";
 
-export const TSP_cities_map = new Map<string, Nodecoordinates>();
-const TSP_cords = [
+const TSP_cords = {
     nodecoordinates10,
     nodecoordinates16,
     nodecoordinates12,
     nodecoordinates21,
     nodecoordinates30,
-].sort((a, b) => a.length - b.length);
-TSP_cords.forEach((nodecoordinates, i) => {
-    TSP_cities_map.set(
-        `序号:${i},规模:${nodecoordinates.length}`,
-        nodecoordinates
-    );
-});
+};
+const entries: [string, Nodecoordinates][] = Object.entries(TSP_cords)
+    .sort((a, b) => a[1].length - b[1].length)
+    .map((entry) => {
+        const name = entry[0];
+        const scale = entry[1].length;
+        return [`名称:${name},规模:${scale}`, entry[1]];
+    });
+export const TSP_cities_map = new Map<string, Nodecoordinates>(entries);
