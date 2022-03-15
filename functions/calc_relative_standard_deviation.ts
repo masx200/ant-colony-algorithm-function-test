d8c8b6c770f2a3b150cae3096f874c8c94ab2601
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-15 15:24:27 +0800

    Update calc_relative_standard_deviation.ts

--- functions/calc_relative_standard_deviation.ts
+++ functions/calc_relative_standard_deviation.ts
@@ -1,13 +1,12 @@
 import { sum } from "lodash";
 import { asserttrue } from "../test/asserttrue";
-/**相对标准差 */
+/**相对一次标准差 */
 export function calc_relative_standard_deviation(arg0: number[]): number {
     asserttrue(arg0.length > 0);
     const length = arg0.length;
     const avarage = sum(arg0) / length;
 
-    const Standard_Deviation = Math.sqrt(
-        sum(arg0.map((a) => Math.pow(a - avarage, 2))) / length
-    );
+    const Standard_Deviation =
+        sum(arg0.map((a) => Math.abs(a - avarage))) / length;
     return Standard_Deviation / avarage;
 }
