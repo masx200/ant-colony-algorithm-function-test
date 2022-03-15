b0c6c666d74705f69f907f16ae539ca6a35fc30b
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-15 15:54:09 +0800

    测试

---
+++ test/__tests__/pickRandomOne.ts
@@ -0,0 +1,19 @@
+import assert from "assert";
+import { pickRandomOne } from "../../functions/pickRandomOne";
+
+it("pickRandomOne", () => {
+    assert([7].includes(pickRandomOne([7])));
+    for (let i = 0; i < 5; i++) {
+        const input = [1, 4, 5, 7, 1, 6, 6, 1, 3, 5, 7];
+
+        assert(input.includes(pickRandomOne(input)));
+        assert(
+            input.includes(
+                pickRandomOne(
+                    input,
+                    input.map((a) => 1 / a)
+                )
+            )
+        );
+    }
+});
--- test/__tests__/relative_standard_deviation.ts
+++ test/__tests__/relative_standard_deviation.ts
@@ -1,9 +1,10 @@
 import { calc_relative_standard_deviation } from "../../functions/calc_relative_standard_deviation";
-
-it("relative_standard_deviation", () => {console.log("relative_standard_deviation start");
+//相对一次标准差
+it("relative_standard_deviation", () => {
+    console.log("relative_standard_deviation start");
     expect(0).toEqual(calc_relative_standard_deviation([1, 1, 1]));
-    
+
     const result1 = calc_relative_standard_deviation([1, 3, 4]);
     console.log("relative_standard_deviation of [1,3,4]", result1);
-    expect(result1).toEqual(0.46770717334674267);
+    expect(result1).toEqual(5 / 12);
 });
