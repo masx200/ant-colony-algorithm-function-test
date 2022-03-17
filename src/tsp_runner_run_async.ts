9943568f4c8cded2e4ad7ca362be69e157062e10
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-17 15:26:21 +0800

    Update tsp_runner_run_async.ts

--- src/tsp_runner_run_async.ts
+++ src/tsp_runner_run_async.ts
@@ -1,19 +1,19 @@
 // import { TSPRunner } from "../functions/createTSPrunner";
-import { sleep_requestAnimationFrame_async_or_settimeout } from "./sleep_requestAnimationFrame_async_or_settimeout";
+// import { sleep_requestAnimationFrame_async_or_settimeout } from "./sleep_requestAnimationFrame_async_or_settimeout";
 
 export async function tsp_runner_run_async(
     runner: { runOneRoute: () => Promise<void> },
     roundofsearch: number,
     numberofants: number
 ): Promise<void> {
-    let last_time = Number(new Date());
+    // let last_time = Number(new Date());
     for (let i = 0; i < roundofsearch * numberofants; i++) {
         await runner.runOneRoute();
 
-        let duration = Number(new Date()) - last_time;
-        if (duration > 20) {
-            await sleep_requestAnimationFrame_async_or_settimeout();
-            last_time = Number(new Date());
-        }
+        // let duration = Number(new Date()) - last_time;
+        // if (duration > 20) {
+        //     await sleep_requestAnimationFrame_async_or_settimeout();
+        //     last_time = Number(new Date());
+        // }
     }
 }
