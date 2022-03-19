85595658d0273d7c14f9ccee7cf21b07f1b5f90e
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-19 11:39:28 +0800

    Update tsp_runner_run_async.ts

--- src/tsp_runner_run_async.ts
+++ src/tsp_runner_run_async.ts
@@ -1,19 +1,28 @@
 // import { TSPRunner } from "../functions/createTSPrunner";
 // import { sleep_requestAnimationFrame_async_or_settimeout } from "./sleep_requestAnimationFrame_async_or_settimeout";
 
+import { TSP_Worker_Remote } from "./TSP_Worker_Remote";
+
 export async function tsp_runner_run_async(
-    runner: { runOneRoute: () => Promise<void> },
+    runner: TSP_Worker_Remote,
     roundofsearch: number,
     numberofants: number
 ): Promise<void> {
-    // let last_time = Number(new Date());
-    for (let i = 0; i < roundofsearch * numberofants; i++) {
-        await runner.runOneRoute();
+    let rest_count = roundofsearch * numberofants;
+    let run_count = 1;
 
-        // let duration = Number(new Date()) - last_time;
-        // if (duration > 20) {
-        //     await sleep_requestAnimationFrame_async_or_settimeout();
-        //     last_time = Number(new Date());
-        // }
+    while (rest_count > 0) {
+        let last_time = Number(new Date());
+        await runner.runRoutes(run_count);
+        rest_count -= run_count;
+        let duration = Number(new Date()) - last_time;
+        if (duration > 20) {
+            run_count = Math.round(Math.max(run_count / 2, 1));
+
+            //     await sleep_requestAnimationFrame_async_or_settimeout();
+            //     last_time = Number(new Date());
+        } else {
+            run_count++;
+        }
     }
 }
