7a93986ffd186e4921d5b31daf81a07fdcd3b3d1
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-22 00:05:59 +0800

    Update defaultnumber_of_ants.ts

--- src/defaultnumber_of_ants.ts
+++ src/defaultnumber_of_ants.ts
@@ -1,7 +1,10 @@
 export const defaultnumber_of_ants = 15;
 export const defaultsearchrounds = 100;
 export const default_local_pheromone_volatilization_rate = 0.035;
-export const default_global_pheromone_volatilization_rate = 0.3;
+
+//由局部信息素挥发率决定全局信息素挥发率
+
+//export const default_global_pheromone_volatilization_rate = 0.3;
 // export const default_searchloopcountratio = 15;
 export const default_alpha = 1;
 export const default_beta = 5;
