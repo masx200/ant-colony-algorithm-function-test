648db2ba3c839aa25f44c01fcb01cdc554b591cd
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-21 16:00:53 +0800

    Update defaultnumberofants.ts

--- src/defaultnumberofants.ts
+++ src/defaultnumberofants.ts
@@ -1,5 +1,5 @@
-export const defaultnumberofants = 10;
-export const defaultsearchrounds = 10;
+export const defaultnumberofants = 15;
+export const defaultsearchrounds = 100;
 export const default_local_pheromone_volatilization_rate = 0.035;
 export const default_global_pheromone_volatilization_rate = 0.3;
 // export const default_searchloopcountratio = 15;
