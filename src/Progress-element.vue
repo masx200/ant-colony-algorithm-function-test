a946cae9b6d918ed9e1837385d6edc086318dacf
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-21 14:42:53 +0800

    rename

---
+++ src/Progress-element.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="fixed-top-navbar Translucent-background">
+        <el-progress :percentage="percentage" textInside :strokeWidth="20" />
+    </div>
+</template>
+<script lang="ts">
+import { ElProgress } from "element-plus";
+import { defineComponent } from "vue";
+export default defineComponent({
+    components: { ElProgress },
+    props: { percentage: { type: Number, required: true } },
+});
+</script>
+<style scoped>
+.fixed-top-navbar {
+    height: 50px;
+    position: fixed;
+    width: 100%;
+    top: 0%;
+    left: 0;
+}
+</style>
