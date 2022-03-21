d122fa331b413f79fba04dfb6bf70afaea6f0083
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-22 00:19:52 +0800

    Update TSPRunnerOptions.ts

--- src/TSPRunnerOptions.ts
+++ src/TSPRunnerOptions.ts
@@ -1,9 +1,12 @@
 import { NodeCoordinates } from "../functions/NodeCoordinates";
 
 export type TSPRunnerOptions = {
+min_coefficient_of_pheromone_diffusion:number
+max_coefficient_of_pheromone_diffusion:number
+
     max_results_of_k_opt?: number | undefined;
     pheromone_volatility_coefficient_R1?: number | undefined;
-    pheromone_volatility_coefficient_R2?: number | undefined;
+ //   pheromone_volatility_coefficient_R2?: number | undefined;
     pheromone_intensity_Q?: number | undefined;
     node_coordinates: NodeCoordinates;
     alpha_zero?: number | undefined;
