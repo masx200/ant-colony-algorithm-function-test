994a575035c57b162c63d97f105ef49040103836
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-23 10:41:43 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,22 @@ app.mount(appcontainer);
         //@ts-ignore
         const { registerSW } = await import("virtual:pwa-register");
 
-        registerSW({});
+        if (
+            location.hostname !== "localhost" &&
+            "127.0.0.1" !== location.hostname
+        ) {
+            "serviceWorker" in navigator &&
+                window.addEventListener(
+                    "load",
+                    function () {
+                        const updateSW = registerSW({
+                            onNeedRefresh() {},
+                            onOfflineReady() {},
+                        });
+                        updateSW(true).catch(() => {});
+                    },
+                    { once: true }
+                );
+        }
     }
 })();
