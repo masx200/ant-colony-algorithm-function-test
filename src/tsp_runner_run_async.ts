b4706d7d9716dd6438e5e9b8dcb4803dee75d5a0
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-25 15:30:51 +0800

    Update tsp_runner_run_async.ts

--- src/tsp_runner_run_async.ts
+++ src/tsp_runner_run_async.ts
@@ -1,46 +1,61 @@
 // import { TSPRunner } from "../functions/createTSPrunner";
+import { RunWay } from "./RunWay";
 import { sleep_requestAnimationFrame_async_or_settimeout } from "./sleep_requestAnimationFrame_async_or_settimeout";
 
 import { TSP_Worker_Remote } from "./TSP_Worker_Remote";
-
+/** time_of_search 优先于 count_of_search 优先于 round_of_search*/
 export async function tsp_runner_run_async({
     runner,
+    time_of_search_ms = Infinity,
     count_of_search = Infinity,
     round_of_search = Infinity,
     number_of_ants,
     onprogress,
 }: {
+    time_of_search_ms?: number; //毫秒
     count_of_search?: number;
     runner: TSP_Worker_Remote;
     round_of_search?: number;
     number_of_ants: number;
     onprogress?: (percentage: number) => void;
 }): Promise<void> {
+    const all_time = time_of_search_ms;
     onprogress && onprogress(0);
     const all_count = count_of_search ?? round_of_search * number_of_ants;
     let rest_count = all_count;
-    let run_count = 20;
+    let run_count = 15;
     const min_count = 10;
     const interval = 3000;
-    while (rest_count > 0) {
-        run_count = Math.min(run_count, rest_count);
-        let last_time = Number(new Date());
-        await runner.runRoutes(run_count);
-        rest_count -= run_count;
-        let duration = Number(new Date()) - last_time;
 
-        // console.log("tsp_runner_run_async,次数", run_count);
-        // console.log("tsp_runner_run_async,用时", duration);
+    let rest_time = time_of_search_ms;
+    const type_of_search =
+        time_of_search_ms < Infinity ? RunWay.time : RunWay.round;
+    let duration: number = 0;
+    while (type_of_search === RunWay.time ? rest_time > 0 : rest_count > 0) {
+        if (type_of_search === RunWay.round) {
+            run_count = Math.min(run_count, rest_count);
+            const last_time = Number(new Date());
+            await runner.runRoutes(run_count);
+            rest_count -= run_count;
+            duration = Number(new Date()) - last_time;
+
+            // console.log("tsp_runner_run_async,次数", run_count);
+            // console.log("tsp_runner_run_async,用时", duration);
+
+            onprogress && onprogress(100 * (1 - rest_count / all_count));
+        } else {
+            const last_time = Number(new Date());
+            await runner.runRoutes(run_count);
+            duration = Number(new Date()) - last_time;
+            rest_time -= duration;
+            onprogress && onprogress(100 * (1 - rest_time / all_time));
+        }
         if (duration > interval) {
             run_count = Math.round(Math.max(run_count / 2, min_count));
-
-            //     await sleep_requestAnimationFrame_async_or_settimeout();
-            //     last_time = Number(new Date());
         } else {
             run_count++;
             run_count = Math.max(min_count, run_count);
         }
-        onprogress && onprogress(100 * (1 - rest_count / all_count));
         await sleep_requestAnimationFrame_async_or_settimeout();
     }
 }
