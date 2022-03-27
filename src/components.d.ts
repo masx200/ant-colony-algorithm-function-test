c3a7ad7b4e10786de88f211c9f9b0f36ca65b9a5
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-27 10:50:33 +0800

    格式化

--- src/components.d.ts
+++ src/components.d.ts
@@ -2,13 +2,13 @@
 // We suggest you to commit this file into source control
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 
-declare module 'vue' {
-  export interface GlobalComponents {
-    ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
-    ElProgress: typeof import('element-plus/es')['ElProgress']
-    ElRadio: typeof import('element-plus/es')['ElRadio']
-    ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
-  }
+declare module "vue" {
+    export interface GlobalComponents {
+        ElInputNumber: typeof import("element-plus/es")["ElInputNumber"];
+        ElProgress: typeof import("element-plus/es")["ElProgress"];
+        ElRadio: typeof import("element-plus/es")["ElRadio"];
+        ElRadioGroup: typeof import("element-plus/es")["ElRadioGroup"];
+    }
 }
 
-export { }
+export {};
