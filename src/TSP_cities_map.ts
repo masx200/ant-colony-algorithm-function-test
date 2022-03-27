fd7fb66c9f7be43d6b98cf5032823520d169029f
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-27 09:08:26 +0800

    优化

--- src/TSP_cities_map.ts
+++ src/TSP_cities_map.ts
@@ -10,7 +10,7 @@ const TSP_cords: Record<string, () => Promise<NodeCoordinates>> =
     Object.fromEntries(
         getNames()
             .map((n) => ({ name: n, dimension: getDimension(n) }))
-            .filter(({ dimension }) => dimension <= 2000)
+            .filter(({ dimension }) => dimension <= 1500)
             .map(({ name }) => [name, () => getNodeCoordinates(name)])
     );
 // console.log(TSP_cords);
