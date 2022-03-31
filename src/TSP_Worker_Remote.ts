e8438269a641178a9eeac852cc22dec78ebe4f40
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-01 07:06:48 +0800

    Update TSP_Worker_Remote.ts

--- src/TSP_Worker_Remote.ts
+++ src/TSP_Worker_Remote.ts
@@ -4,8 +4,12 @@ import { DataOfFinishOneIteration } from "../functions/DataOfFinishOneIteration"
 import { DataOfFinishOneRoute } from "../functions/DataOfFinishOneRoute";
 import { TSP_Worker_API } from "./TSP_Worker_API";
 
-export type TSP_Worker_Remote = comlink.RemoteObject<TSP_Worker_API> &
-    comlink.ProxyMethods & { terminate: () => void } & {
+export type TSP_Worker_Remote ={worker:Worker, terminate: () => void } & 
+
+{
+
+remote:comlink.RemoteObject<TSP_Worker_API> &
+    comlink.ProxyMethods &  {
         on_finish_one_route: (
             callback: (data: DataOfFinishOneRoute) => void
         ) => Promise<void>;
@@ -16,3 +20,4 @@ export type TSP_Worker_Remote = comlink.RemoteObject<TSP_Worker_API> &
             callback: (data: DataOfBestChange) => void
         ) => Promise<void>;
     };
+}
