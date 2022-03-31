004cef43ecb92b7959a121124ae9d80493be24ea
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-01 07:12:57 +0800

    Update thread_Greedy_algorithm_to_solve_tsp_with_selected_start.ts

--- src/thread_Greedy_algorithm_to_solve_tsp_with_selected_start.ts
+++ src/thread_Greedy_algorithm_to_solve_tsp_with_selected_start.ts
@@ -3,7 +3,7 @@ import { NodeCoordinates } from "../functions/NodeCoordinates";
 import { createWorkerPool } from "./createWorkerPool";
 import { create_Worker_comlink } from "./create_Worker_comlink";
 import Greedy_algorithm_to_solve_tsp_with_selected_start_Worker from "./Greedy_algorithm_to_solve_tsp_with_selected_start.worker?worker";
-const Greedy_algorithm_to_solve_tsp_with_selected_start_pool = createWorkerPool(
+export const Greedy_algorithm_to_solve_tsp_with_selected_start_pool = createWorkerPool(
     () => {
         return create_Worker_comlink<{
             Greedyalgorithmtosolvetspwithselectedstart: typeof Greedyalgorithmtosolvetspwithselectedstart;
