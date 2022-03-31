c467dea2a6afedfb36c8a41d490c8e7a28bf9e59
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-01 07:58:47 +0800

    Update TSP_workerRef.ts

--- src/TSP_workerRef.ts
+++ src/TSP_workerRef.ts
@@ -1,7 +1,5 @@
-//import { TSP_Worker_Remote } from "./TSP_Worker_Remote";
+import { TSP_Worker_Remote } from "./TSP_Worker_Remote";
 
-// import { TSPRunner } from "../functions/createTSPrunner";
-// export const TSP_workerRef: { value?: Worker | undefined } = {};
 export const TSP_RunnerRef: {
-    value?: {terminate:()=>void} | undefined;
+    value?: TSP_Worker_Remote | undefined;
 } = {};
