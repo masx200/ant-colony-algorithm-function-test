2a494dd4630b83f38b87cb7c0b3cb506f64374ac
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-05 18:06:53 +0800

    greedy_first_search_routes_parallel

--- functions/run_greedy_once_thread.ts
+++ functions/run_greedy_once_thread.ts
@@ -8,14 +8,16 @@ import { thread_Greedy_algorithm_to_solve_tsp_with_selected_start } from "../src
 
 export async function run_greedy_once_thread(
     inputindexs: number[],
-    node_coordinates: NodeCoordinates) {
+    node_coordinates: NodeCoordinates
+): Promise<{ total_length: number; route: number[]; time_ms: number; }> {
     const start_time = Number(new Date());
     const start = getnumberfromarrayofnmber(pickRandomOne(inputindexs));
 
-    const route = await thread_Greedy_algorithm_to_solve_tsp_with_selected_start(
-        node_coordinates,
-        start
-    );
+    const route =
+        await thread_Greedy_algorithm_to_solve_tsp_with_selected_start(
+            node_coordinates,
+            start
+        );
     const greedypath = cycle_reorganize(route, 0);
     const total_length = closedtotalpathlength({
         path: greedypath,
