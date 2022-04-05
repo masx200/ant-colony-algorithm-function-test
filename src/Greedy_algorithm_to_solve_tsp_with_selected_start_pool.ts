1db4a4f723821bcede4784b56a6a830d04a8d11d
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-05 18:18:41 +0800

    Greedy_algorithm_to_solve_tsp_with_selected_start_pool

--- src/Greedy_algorithm_to_solve_tsp_with_selected_start_pool.ts
+++ src/Greedy_algorithm_to_solve_tsp_with_selected_start_pool.ts
@@ -3,20 +3,22 @@ import { createWorkerPool } from "./createWorkerPool";
 import { create_Worker_comlink } from "./create_Worker_comlink";
 import Greedy_algorithm_to_solve_tsp_with_selected_start_Worker from "./Greedy_algorithm_to_solve_tsp_with_selected_start.worker?worker";
 
-export const Greedy_algorithm_to_solve_tsp_with_selected_start_pool = createWorkerPool(
-    () => {
-        return create_Worker_comlink<{
-            Greedyalgorithmtosolvetspwithselectedstart: typeof Greedyalgorithmtosolvetspwithselectedstart;
-        }>(() => {
-            const w = new Greedy_algorithm_to_solve_tsp_with_selected_start_Worker();
-            w.addEventListener("error", (e) => {
-                alert(String(e) + e.message);
-                throw e;
+export const Greedy_algorithm_to_solve_tsp_with_selected_start_pool =
+    createWorkerPool(
+        () => {
+            return create_Worker_comlink<{
+                Greedyalgorithmtosolvetspwithselectedstart: typeof Greedyalgorithmtosolvetspwithselectedstart;
+            }>(() => {
+                const w =
+                    new Greedy_algorithm_to_solve_tsp_with_selected_start_Worker();
+                w.addEventListener("error", (e) => {
+                    alert(String(e) + e.message);
+                    throw e;
+                });
+                return w;
             });
-            return w;
-        });
-    },
-    {
-        size: navigator.hardwareConcurrency,
-    }
-);
+        },
+        {
+            size: navigator.hardwareConcurrency,
+        }
+    );
