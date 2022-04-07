136889bd4bff6db2082471907e6c72960ed2b5b1
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-07 16:48:39 +0800

    whether_2_sections_reverse_opt

--- k-opt/whether_2_sections_reverse_opt.ts
+++ k-opt/whether_2_sections_reverse_opt.ts
@@ -1,21 +1,14 @@
 /** 
-0: (3) [true, true, true]
-1: (3) [true, true, false]
-2: (3) [true, false, true]
-3: (3) [true, false, false]
-4: (3) [false, true, true]
-5: (3) [false, true, false]
-6: (3) [false, false, true]
-7: (3) [false, false, false]
+[1,2,3,4]   [2,1,4,3]
+[1,2,4,3]   [2,1,3,4]
 */
 export function* whether_2_sections_reverse_opt(): Generator<
     [boolean, boolean],
     void,
     unknown
 > {
-    for (let i of [true, false]) {
-        for (let j of [true, false]) {
-            yield [i, j];
-        }
-    }
+    /*      [false, false],等于     [true, true], */
+    /*    [true, false],,等于     [false, true], */
+    yield[true, false];
+    yield[false, false];
 }
