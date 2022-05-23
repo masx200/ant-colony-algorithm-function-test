425310ef5b5b51a84516cd639ef95123629dba17
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-23 14:13:56 +0800

    Create tsp-interface.ts

---
+++ classic-acs/tsp-interface.ts
@@ -0,0 +1,44 @@
+export interface COMMON_TSP_EXECUTION {
+    runOneIteration: () => Promise<void>;
+    get_output_data: () => COMMON_TSP_Output;
+}
+export interface COMMON_TSP_Output {
+    total_time_ms: number;
+    current_iterations: number;
+    current_search_count: number;
+    data_of_routes: COMMON_DataOfOneRoute[];
+    data_of_iterations: COMMON_DataOfOneIteration[];
+    time_of_best_ms: number;
+    global_best_route: number[];
+    global_best_length: number;
+    search_count_of_best: number;
+}
+export interface COMMON_DataOfOneRoute {
+    global_best_length: number;
+
+    current_search_count: number;
+    time_ms_of_one_route: number;
+
+    current_route_length: number;
+}
+export interface COMMON_DataOfOneIteration {
+    current_iterations: number;
+    global_best_length: number;
+    iterate_best_length: number;
+    population_relative_information_entropy: number;
+    average_length_of_iteration: number;
+    worst_length_of_iteration: number;
+    time_ms_of_one_iteration: number;
+}
+export interface COMMON_TSP_Options {
+    distance_round?: boolean;
+    alpha_zero?: number | undefined;
+    beta_zero?: number | undefined;
+    count_of_ants?: number | undefined;
+    node_coordinates: NodeCoordinates;
+    pheromone_intensity_Q?: number;
+    pheromone_volatility_coefficient_local?: number;
+    pheromone_volatility_coefficient_global?: number;
+    route_selection_parameters_Q0?: number;
+}
+import { NodeCoordinates } from "../functions/NodeCoordinates";
