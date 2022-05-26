d96d4b75b755030cf9635b2893cb5a6655349d69
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-26 10:18:31 +0800

    Update tsp_runner_run_async.ts

--- src/tsp_runner_run_async.ts
+++ src/tsp_runner_run_async.ts
@@ -1,7 +1,7 @@
 import { COMMON_TSP_Output } from "../classic-acs/tsp-interface";
 
 import { assert_true } from "../test/assert_true";
-import { drawChartMaxWait } from "./drawChartMaxWait";
+import { drawChartMaxWait, drawChartWait } from "./drawChartMaxWait";
 import { RunWay } from "./RunWay";
 import { sleep_requestAnimationFrame_async_or_settimeout } from "./sleep_requestAnimationFrame_async_or_settimeout";
 import { TSP_Worker_Remote } from "./TSP_Worker_Remote";
@@ -30,10 +30,8 @@ export async function tsp_runner_run_async({
     onprogress && onprogress(0);
     const all_iterations = iterations_of_search;
     let rest_iterations = all_iterations;
-    const run_iterations = 1;
-    // const min_count = 1;
-    // const interval = 10000;
-
+    let run_iterations = 1;
+    const min_count = 1;
     let rest_time = time_of_search_ms;
     const type_of_search =
         time_of_search_ms < Infinity ? RunWay.time : RunWay.round;
@@ -42,7 +40,6 @@ export async function tsp_runner_run_async({
         type_of_search === RunWay.time ? rest_time > 0 : rest_iterations > 0
     ) {
         if (type_of_search === RunWay.round) {
-            // run_iterations = Math.min(run_iterations, rest_iterations);
             const last_time = Number(new Date());
             await runner.runIterations(run_iterations);
             rest_iterations -= run_iterations;
@@ -60,14 +57,15 @@ export async function tsp_runner_run_async({
             onprogress &&
                 onprogress(Math.min(100, 100 * (1 - rest_time / all_time)));
         }
-        // if (duration > interval) {
-        //     run_iterations = Math.round(
-        //         Math.max(run_iterations / 2, min_count)
-        //     );
-        // } else {
-        //     run_iterations++;
-        //     run_iterations = Math.max(min_count, run_iterations);
-        // }
+        if (duration > drawChartWait) {
+            run_iterations = Math.max(
+                Math.round(run_iterations / 2),
+                min_count
+            );
+        } else {
+            run_iterations++;
+            run_iterations = Math.min(run_iterations, rest_iterations);
+        }
 
         if (Number(new Date()) - last_update_time > drawChartMaxWait) {
             on_update_output_data(await runner.get_output_data());
