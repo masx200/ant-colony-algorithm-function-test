b9fd3cf678310a4a5f39eb782e02055d32476ce0
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-26 18:09:57 +0800

    优化轮盘选择

--- __tests__/test-pickRandomOne.ts
+++ __tests__/test-pickRandomOne.ts
@@ -2,10 +2,21 @@ import assert from "assert";
 import { pickRandomOne } from "../functions/pickRandomOne";
 
 it("pickRandomOne", () => {
-    assert([7].includes(pickRandomOne([7])));
-    for (let i = 0; i < 5; i++) {
-        const input = [1, 4, 5, 7, 1, 6, 6, 1, 3, 5, 7];
-
+    for (let i = 0; i < 50; i++) {
+        assert([7].includes(pickRandomOne([7])));
+        const input = [1, 4, 5, 7, 1, 6, 6, 1, 3, 5, 7, 10, 12, 30, 20, 18];
+        assert(
+            pickRandomOne(
+                input,
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ) === 1
+        );
+        assert(
+            pickRandomOne(
+                input,
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+            ) === 18
+        );
         assert(input.includes(pickRandomOne(input)));
         assert(
             input.includes(
--- functions/pickRandomOne.ts
+++ functions/pickRandomOne.ts
@@ -9,16 +9,47 @@ export function pickRandomOne<T>(input: Array<T>, weights: number[] = []): T {
     if (weights.length) {
         assert_true(weights.length === input.length);
         const sumofweights = sum(weights);
-        const Roulette = weights.map((_w, i, a) => {
-            return sum(a.slice(0, i + 1)) / sumofweights;
-        });
+        const Roulette = Array.from(weights);
+
+        let i = 0;
+        for (const w of weights) {
+            Roulette[i] = Math.min(
+                1,
+                w / sumofweights + (Roulette[i - 1] ?? 0)
+            );
+            i++;
+        }
+        // console.log(Roulette)
+        assert_true(Roulette.every((n) => n >= 0 && n <= 1));
         const ran = Math.random();
-        for (let i = 0; i < input.length; i++) {
-            if (ran < Roulette[i]) {
-                const result = input[i];
+        // for (let i = 0; i < input.length; i++) {
+        //     if (ran < Roulette[i]) {
+        //         const result = input[i];
+        //         return result;
+        //     }
+        // }
+        if (ran <= Roulette[0]) {
+            const result = input[0];
+            return result;
+        }
+        if (ran >= (Roulette.at(-1) || 1)) {
+            const result = input[input.length - 1];
+            return result;
+        }
+        let left = 0;
+        let right = input.length - 1;
+        while (right - left >= 1) {
+            const mid = Math.floor((right + left) / 2);
+            if (ran <= Roulette[mid] && (Roulette[mid - 1] ?? 0) <= ran) {
+                const result = input[mid];
                 return result;
+            } else if (ran < (Roulette[mid - 1] ?? 0)) {
+                right = mid - 1;
+            } else if (ran > Roulette[mid]) {
+                left = mid + 1;
             }
         }
+
         const last = input.at(-1);
         assert_true(typeof last !== "undefined");
         return last;
