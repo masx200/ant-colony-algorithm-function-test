1462c57f3cc1b938dec6d4227460722c2f578b49
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-27 13:37:35 +0800

    Update select_available_cities_from_optimal_and_latest.ts

--- functions/select_available_cities_from_optimal_and_latest.ts
+++ functions/select_available_cities_from_optimal_and_latest.ts
@@ -1,5 +1,5 @@
-import intersection from "lodash/intersection";
-import difference from "lodash/difference";
+// import intersection from "lodash/intersection";
+// import difference from "lodash/difference";
 // import { ArrayShuffle } from "./ArrayShuffle";
 
 export function select_available_cities_from_optimal_and_latest({
@@ -14,20 +14,45 @@ export function select_available_cities_from_optimal_and_latest({
     ) => number[];
     current_city: number;
     max_size_of_cities: number;
-}): number[] {
+}): number[] | Set<number> {
     const maximum = Math.max(max_size_of_cities, available_nodes.size);
-
-    const neighbors = intersection(
-        get_neighbors_from_optimal_routes_and_latest_routes(current_city),
-        Array.from(available_nodes)
-    );
     const slice_from_start = Math.random() < 0.5;
-    let rest_nodes = difference(Array.from(available_nodes), neighbors);
-    rest_nodes = slice_from_start
-        ? rest_nodes.slice(0, maximum - neighbors.length)
-        : rest_nodes.slice(-maximum - neighbors.length);
-    const source = [...neighbors, ...rest_nodes]; //.flat();
-    const result: number[] = source;
-    // const result: number[] = source.slice(0, maximum);
+    // const neighbors = intersection(
+    //     get_neighbors_from_optimal_routes_and_latest_routes(current_city),
+    //     Array.from(available_nodes)
+    // );
+
+    // let rest_nodes = difference(Array.from(available_nodes), neighbors);
+    // rest_nodes = slice_from_start
+    //     ? rest_nodes.slice(0, maximum - neighbors.length)
+    //     : rest_nodes.slice(-maximum - neighbors.length);
+    const source = new Set<number>();
+    for (const city of get_neighbors_from_optimal_routes_and_latest_routes(
+        current_city
+    )) {
+        if (available_nodes.has(city)) {
+            source.add(city);
+        }
+    }
+    const rest_nodes = Array.from(available_nodes);
+    if (slice_from_start) {
+        for (let i = 0; i < rest_nodes.length; i++) {
+            if (source.size < maximum) {
+                source.add(rest_nodes[i]);
+            } else {
+                break;
+            }
+        }
+    } else {
+        for (let i = rest_nodes.length - 1; i >= 0; i--) {
+            if (source.size < maximum) {
+                source.add(rest_nodes[i]);
+            } else {
+                break;
+            }
+        }
+    }
+    const result = Array.from(source);
+    // const result: number[] = Array.from(source).slice(0, maximum);
     return result;
 }
