bb788b5bf35fe8e42466d8fc8c97cfeef6dddbe6
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-06 14:26:49 +0800

    tsp_acs_execution_with_dynamic_pheromone

--- src/ant_colony_algorithms_to_creator.ts
+++ src/ant_colony_algorithms_to_creator.ts
@@ -5,28 +5,24 @@ import {
 import { tsp_acs_execution } from "../classic-acs/tsp_acs_execution";
 import { tsp_acs_execution_and_local_optimization } from "../classic-acs/tsp_acs_execution_and_local_optimization";
 import { tsp_acs_execution_and_local_optimization_with_Optional_city_rewards_and_punishments } from "../classic-acs/tsp_acs_execution_and_local_optimization_with_Optional-city-rewards-and-punishments";
+import { tsp_acs_execution_with_dynamic_pheromone } from "../classic-acs/tsp_acs_execution_with_dynamic_pheromone";
 import { assert_true } from "../test/assert_true";
 import { ant_colony_algorithms } from "./ant_colony_algorithms";
-// import { ant_colony_algorithms_to_creator } from "./ant_colony_algorithms_to_creator";
+
 export const ant_colony_algorithms_to_creator: Record<
     string,
     () => (options: COMMON_TSP_Options) => COMMON_TSP_EXECUTION
 > = {
     "经典蚁群算法,ACS": function () {
-        // const { tsp_acs_execution } = await import(
-        //     "../classic-acs/tsp_acs_execution"
-        // );
         return tsp_acs_execution;
     },
     "ACS+三种局部优化方法": function () {
-        // const { tsp_acs_execution_and_local_optimization } = await import(
-        //     "../classic-acs/tsp_acs_execution_and_local_optimization"
-        // );
         return tsp_acs_execution_and_local_optimization;
     },
     "ACS+三种局部优化方法+最优路径集合的可选城市奖惩": function () {
         return tsp_acs_execution_and_local_optimization_with_Optional_city_rewards_and_punishments;
     },
+    "ACS+动态信息素": () => tsp_acs_execution_with_dynamic_pheromone,
 };
 assert_true(
     ant_colony_algorithms.every((algorithm) => {
