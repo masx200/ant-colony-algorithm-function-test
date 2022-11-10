07db27d268e97f909ee314eadfe9c0031920e2b8
masx200 <masx200@qq.com>
2022-11-10 16:35:48 +0800

    删除无用文件

--- functions/isDataOfFinishOneIteration.ts
+++
@@ -1,8 +0,0 @@
-import { DataOfFinishOneIteration } from "./DataOfFinishOneIteration";
-import { DataOfFinishOneRoute } from "./DataOfFinishOneRoute";
-
-export function isDataOfFinishOneIteration(
-    data: DataOfFinishOneIteration | DataOfFinishOneRoute | undefined
-): data is DataOfFinishOneIteration {
-    return typeof (data as any)?.current_iterations === "number";
-}
--- functions/isDataOfFinishOneRoute.ts
+++
@@ -1,8 +0,0 @@
-import { DataOfFinishOneIteration } from "./DataOfFinishOneIteration";
-import { DataOfFinishOneRoute } from "./DataOfFinishOneRoute";
-
-export function isDataOfFinishOneRoute(
-    data: DataOfFinishOneIteration | DataOfFinishOneRoute | undefined
-): data is DataOfFinishOneRoute {
-    return typeof (data as any)?.current_search_count === "number";
-}
